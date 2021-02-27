98b9fe0d2cf8d476198d5a39435e0036062edee6
i7or <bailov1ab@gmail.com>
2021-02-27 19:25:28 +0200

    Addition Entity Framework

---
+++ Server/UserContextDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class UserContextDB : DbContext
+    {
+        public UserContextDB() : base("DbConnection") {}
+
+        public DbSet<UserDB> users { get; set; }
+    }
+}
